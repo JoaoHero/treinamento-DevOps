1cc81ab9237d99da8e6e7102dde6cabee5dad86c
Joao Ramos <joao.silvaramos2013@hotmail.com>
2023-06-16 20:33:01 -0300

    Selecionado todas as folders e files do nosso projeto, criado no index um container com 3 divs e no js uma function para identificar qual elemento e clicado

---
+++ content/script.js
@@ -0,0 +1,17 @@
+const container = document.querySelectorAll(".container-wrapper div");
+
+function elementSelected() {
+    container.forEach(function(element, index){
+        element.addEventListener("click", function() {
+            if(index === 0) {
+                console.log("Clicou na div1");
+            }else if(index === 1) {
+                console.log("Clicou na div2");
+            }else {
+                console.log("Clicou na div3");
+            }
+        })
+    })
+}
+
+elementSelected();
